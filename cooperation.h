5278bbbbdaa6374b8974ed2140ad50d781e1222c
mansuimo <17301310870@163.com>
2026-09-20 15:33:35 +0800

    feat: 新增 cooperation 模块，提供欢迎/求和/分隔线函数

---
+++ cooperation.h
@@ -0,0 +1,13 @@
+#ifndef COOPERATION_H
+#define COOPERATION_H
+
+// 打印欢迎信息
+void printWelcome(const char* name);
+
+// 计算两个整数的和并返回
+int add(int a, int b);
+
+// 打印 n 个星号组成的分隔线
+void printSeparator(int n);
+
+#endif
