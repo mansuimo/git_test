119bb7b51dfcef3acd134645605c4f50e0b6c6f6
mansuimo <17301310870@163.com>
2026-09-20 15:33:47 +0800

    refactor: General_Education 改为调用 cooperation 中的函数

--- General_Education.cpp
+++ General_Education.cpp
@@ -1,8 +1,14 @@
 #include <stdio.h>
+#include "cooperation.h"   
 
 int main(){
     char name[] = "Git Learning";
-    printf("%s!",name);
-    
+
+    // 调用 cooperation.cpp 里实现的函数
+    printSeparator(30);
+    printWelcome(name);
+    printf("1 + 2 = %d\n", add(1, 2));
+    printSeparator(30);
+
     return 0;
 }
